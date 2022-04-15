1f30824e615c6424b85106e1707e652c517f3a4e
wachcio <wachcio@poczta.fm>
2022-04-15 15:48:04 +0200

    fix getYearlyProduction

--- backend/routes/getYearlyProduction.ts
+++ backend/routes/getYearlyProduction.ts
@@ -8,9 +8,6 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(timezone);
 import mysql from 'mysql';
 
-import axios from 'axios';
-import _ from 'lodash';
-
 dayjs.tz.setDefault('Europe/Warsaw');
 
 router.get('/', async (req, res, next) => {
@@ -30,7 +27,6 @@ router.get('/', async (req, res, next) => {
 
   // console.log('API');
   const connection = mysql.createConnection({
-    connectionLimit: 1,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -45,7 +41,7 @@ router.get('/', async (req, res, next) => {
   //SET @row_number:=0; SELECT @row_number:=DAY(`timestamp`) AS Day, (MAX(`value`)-MIN(`value`)) AS sum FROM `total_production` WHERE YEAR(`timestamp`) = 2021 AND MONTH(`timestamp`) = 7 GROUP BY YEAR(`timestamp`), MONTH(`timestamp`), DAY(`timestamp`)
 
   const getYearlyProductionFromDatabase = ({ year }) => {
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       // const query = `SELECT * FROM \`${process.env.DB_TABLE_DETAILED_DATA}\` WHERE YEAR( \`timestamp\` ) = ${year} AND MONTH(\`timestamp\`)=${month} AND DAY(\`timestamp\`)=${day}`;
       const query = `SELECT @row_number:=MONTH(\`timestamp\`) AS Month, (MAX(\`value\`)-MIN(\`value\`)) AS Production FROM \`${process.env.DB_TABLE_TOTAL_PRODUCTION}\` WHERE YEAR(\`timestamp\`) = ${year} GROUP BY YEAR(\`timestamp\`), MONTH(\`timestamp\`)`;
 
