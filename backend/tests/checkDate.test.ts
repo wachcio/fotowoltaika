7ae20c8620b58fe0e290512c6610c65d04b28ee6
wachcio <wachcio@poczta.fm>
2022-04-20 16:20:37 +0200

    cleaning

--- backend/tests/checkDate.test.ts
+++ backend/tests/checkDate.test.ts
@@ -5,3 +5,15 @@ import { checkDate } from '../helpers/checkDate';
 test('Check date: 01-01-2022', () => {
   expect(checkDate({ year: 2022, month: 1, day: 1 })).toBeFalsy();
 });
+test('Check date: 01-01-2021', () => {
+  expect(checkDate({ year: 2021, month: 1, day: 1 })).toBeFalsy();
+});
+test('Check date: 01-01-2020', () => {
+  expect(checkDate({ year: 2020, month: 1, day: 1 })).toBeTruthy();
+});
+test('Check date: 00-01-2020', () => {
+  expect(checkDate({ year: 2020, month: 1, day: 0 })).toBeTruthy();
+});
+test('Check date: 00-01-20200', () => {
+  expect(checkDate({ year: 20200, month: 1, day: 0 })).toBeTruthy();
+});
