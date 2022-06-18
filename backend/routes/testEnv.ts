81d3e7e782ebacb1bed2949c8dabc48ec6d98c91
wachcio <wachcio@poczta.fm>
2022-06-18 22:03:59 +0200

    Remove reset .env route

--- backend/routes/testEnv.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', async (req, res, next) => {
-  res.status(200).json({ host: 'test' }); // send a json response
-});
-
-export { router as testEnv };
